45c919306cc6d17b56f01b2eec298f7ae78a881d
Arogya-2002 <arogyavamshi2002@gmail.com>
2025-01-27 15:29:43 +0530

    updated template

--- template.py
+++ template.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
 project_name="textSummarizer"
+
 
 list_of_files=[
     ".github/workflows/.gitkeep",
